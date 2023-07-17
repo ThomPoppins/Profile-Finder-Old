cf0ae3e40f64880192cbae24e4e355e3e33180ec
Thom Poppins <thompoppins@gmail.com>
2023-07-18 00:47:44 +0100

    initiated logrocket for debugging

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,25 @@
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
+import LogRocket from 'logrocket';
 
 export default function App({
   Component, pageProps: { session, ...pageProps }
 }) {
   return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <div>
+      {LogRocket.init('ti1uwj/profile-finder-hmwx0')};
+      {// This is an example script - don't forget to change it!
+        LogRocket.identify('THE_USER_ID_IN_YOUR_APP', {
+          name: 'Thom Poppins',
+          email: 'thompoppins@example.com',
+
+          // Add your own custom user variables here, ie:
+          subscriptionType: 'pro'
+        })}
+
+      <SessionProvider session={pageProps.session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </div>
   )
 }
